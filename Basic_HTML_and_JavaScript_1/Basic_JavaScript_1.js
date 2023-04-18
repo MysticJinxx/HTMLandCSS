42e44c5c90d2cd57e29e7491feeb2b88e74b27f7
Joshua Dishong <dishong.joshua@yahoo.com>
2023-04-18 17:49:14 -0400

    Fixed some typos and added ending bracket to and html tag

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,5 +1,5 @@
-alert("Hello, Wolrd!");
-	function My_First_Function() {
+alert("Hello, Wolrd!");	
+function My_First_Function() {
 		var String = "Kiss me, I'm Irish!";
 		var result = String.fontcolor("green");
 		document.getElementById("Irish").innerHTML = 
